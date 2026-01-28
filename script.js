78be8aba23680c7961cc8916d5e82e46cbbdd5c6
Tanushree-122 <tanushreesaxena34@gmail.com>
2026-01-28 16:25:45 +0530

    Changes in js file

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+console.error("This is an error message");
+console.warn("This is a warning message");
+let x=23;
+x=25;
+// const y=12;
+// var z=19;
+// z=34;
+// y=13;
